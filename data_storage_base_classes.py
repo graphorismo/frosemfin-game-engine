b4ce793c11b86feba01e343914c836f01ee1ad96
graphorismo <graphorismo@gmail.com>
2021-08-09 22:52:50 +0300

    1. Add .gitignore with patter to ignore PyCharm project files 2. Add common_classes.py file which includes base entites data classes 3. Add data_storage_base_classes.py which includes interface for realisation of database engine 4. Add gui_engine_base_classes.py which includes interface for realisation of gui engine

---
+++ data_storage_base_classes.py
@@ -0,0 +1,40 @@
+from enum import Enum
+
+from common_classes import *
+
+
+class IDataStorage:
+
+    # Functions to override in inherited classes:
+
+    def get_data_of_all_projectiles(self) -> list[ProjectileData]:
+        raise RuntimeError("Can't find override for function"
+                           " get_data_of_projectiles(...) from interface IDataStorage")
+
+    def edit_or_create_data_of_projectiles(self, projectiles: list[ProjectileData]):
+        raise RuntimeError("Can't find override for function"
+                           " edit_or_create_data_of_projectiles(...) from interface IDataStorage")
+
+    def get_data_of_all_bodies_of_bots(self) -> list[BodyData]:
+        raise RuntimeError("Can't find override for function"
+                           " get_data_of_bodies_of_bots(...) from interface IDataStorage")
+
+    def edit_or_create_data_of_bodies_of_bots(self, bodies_of_bots: list[BodyData]):
+        raise RuntimeError("Can't find override for function"
+                           " edit_or_create_data_of_bodies_of_bots(...) from interface IDataStorage")
+
+    def get_data_of_all_bodies_of_obstacles(self) -> list[BodyData]:
+        raise RuntimeError("Can't find override for function"
+                           " get_data_of_all_bodies_of_obstacles(...) from interface IDataStorage")
+
+    def edit_or_create_data_of_bodies_of_obstacles(self, bodies_of_bots: list[BodyData]):
+        raise RuntimeError("Can't find override for function"
+                           " edit_or_create_data_of_bodies_of_obstacles(...) from interface IDataStorage")
+
+    def get_data_of_the_body_of_the_player(self) -> BodyData:
+        raise RuntimeError("Can't find override for function"
+                           " get_data_of_the_body_of_the_player(...) from interface IDataStorage")
+
+    def edit_the_player_body_data(self, player_body_data: BodyData):
+        raise RuntimeError("Can't find override for function"
+                           " edit_the_player_body_data(...) from interface IDataStorage")
---
+++ gui_engine_base_classes.py
@@ -0,0 +1,26 @@
+from common_classes import *
+
+
+class IGUIDrawer:
+
+    # Functions to override in inherited classes:
+
+    def draw_an_obstacle(self, obstacle_data: BodyData):
+        raise RuntimeError("Can't find override for function"
+                           " draw_an_obstacle(...) from interface IGUIDrawer")
+
+    def draw_a_projectile(self, projectile_data: ProjectileData):
+        raise RuntimeError("Can't find override for function"
+                           " draw_a_projectile(...) from interface IGUIDrawer")
+
+    def draw_a_bot(self, bot_data: BodyData):
+        raise RuntimeError("Can't find override for function"
+                           " draw_a_bot(...) from interface IGUIDrawer")
+
+    def draw_a_player(self, player_data: BodyData):
+        raise RuntimeError("Can't find override for function"
+                           " draw_a_player(...) from interface IGUIDrawer")
+
+    def clear_screen(self):
+        raise RuntimeError("Can't find override for function"
+                           " clear_screen(...) from interface IGUIDrawer")
