ddf7b43015b235c44d0faa57bc5db0080a2f1e3c
Фома Гришечко <tomasgrish@MacBook-Air-Foma.local>
2021-08-11 17:20:34 +0300

    GUI can draw grid now

--- GUI_for_stupid_bots_world.py
+++ GUI_for_stupid_bots_world.py
@@ -1,6 +1,14 @@
+"""
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
+#from PyQt5.QtWidgets import QApplication, QMainWindow
+"""
+from gui_engine_base_classes import IGUIDrawer
+from common_classes import *
+from time import sleep
 
+import turtle
+
+"""
 import sys
 
 class Window(QMainWindow):
@@ -13,13 +21,78 @@ class Window(QMainWindow):
         self.main_text = QtWidgets.QLabel(self)
         self.main_text.setText("ВЫ ЗАПУСТИЛИ ТАНКИ ОНЛАЙН")
         self.main_text.adjustSize()
+"""
+class GUIActions(IGUIDrawer):
+    def __init__(self, x, squarex, squarey):
+        self.x = x
+        self.squarex = squarex
+        self.squarey = squarey
+        self.len_square = self.x / self.squarex
+        self.y = int(self.squarey * self.len_square)
+        print(self.x, self.y, self.squarex, self.squarey)
 
+    def initialize(self):
 
-def application():
-    app = QApplication(sys.argv)
-    window = Window()
-    window.show()
-    sys.exit(app.exec_())
+        """
+        app = QApplication(sys.argv)
+        window = Window()
+        window.show()
+        sys.exit(app.exec_())
+        """
+        turtle.tracer(1, 0)
+        turtle.reset()
+        turtle.screensize(self.x, self.y)
 
+        turtle.up()
+        turtle.goto(0-(self.x/2), 0-(self.y/2))
+        turtle.setheading(0)
 
-application()
+        for i in range(self.squarex + 1):
+            turtle.down()
+            turtle.left(90)
+            turtle.forward(self.y)
+            turtle.back(self.y)
+            turtle.right(90)
+            turtle.forward(self.len_square)
+
+        turtle.up()
+        turtle.goto(0 - (self.x / 2), 0 + (self.y / 2))
+        turtle.setheading(0)
+
+        for i in range(self.squarey + 1):
+            turtle.down()
+            turtle.forward(self.x)
+            turtle.back(self.x)
+            turtle.right(90)
+            turtle.forward(self.len_square)
+            turtle.left(90)
+
+        sleep(3)
+
+    def draw_an_obstacle(self, obstacle_data: BodyData):
+        raise RuntimeError("Can't find override for function"
+                           " draw_an_obstacle(...) from interface IGUIDrawer")
+
+    def draw_a_projectile(self, projectile_data: ProjectileData):
+        raise RuntimeError("Can't find override for function"
+                           " draw_a_projectile(...) from interface IGUIDrawer")
+
+    def draw_a_bot(self, bot_data: BodyData):
+        raise RuntimeError("Can't find override for function"
+                           " draw_a_bot(...) from interface IGUIDrawer")
+
+    def draw_a_player(self, player_data: BodyData):
+        raise RuntimeError("Can't find override for function"
+                           " draw_a_player(...) from interface IGUIDrawer")
+
+    def clear_screen(self):
+        raise RuntimeError("Can't find override for function"
+                           " clear_screen(...) from interface IGUIDrawer")
+
+    def stop(self):
+        raise RuntimeError("Can't find override for function"
+                           " stop(...) from interface IGUIDrawer")
+
+actions = GUIActions(600, 10, 6)
+actions.initialize()
+
