a312a468e809e3e3e8634098a9c2351d01abfb24
graphorismo <graphorismo@gmail.com>
2021-08-12 22:29:49 +0300

    Do some refactors in PlayerController::react_to_the_last_players_action(...) to improve readability

--- player_controller_base_classes.py
+++ player_controller_base_classes.py
@@ -2,14 +2,12 @@ from common_classes import *
 
 
 class PlayerActionTypeEnum(Enum):
-
     NOTHING = 0,
     MOVE = 1,
     SHOOT = 2
 
 
 class PlayerAction:
-
     direction: DirectionEnum = DirectionEnum.NO_DIRECTION
     action_type: PlayerActionTypeEnum = PlayerActionTypeEnum.NOTHING
 
@@ -20,36 +18,38 @@ class IKeyboard:
         raise RuntimeError("Can't find override for function"
                            " get_the_last_player_action(...) from interface IKeyboard")
 
+
 class PlayerController:
+    keyboard: IKeyboard
 
-     keyboard: IKeyboard
+    def __init__(self, keyboard: IKeyboard):
+        self.keyboard = keyboard
 
-     def __init__(self, keyboard: IKeyboard):
-         self.keyboard = keyboard
-
-     def react_to_the_last_players_action(self, player_data : BodyData):
-         last_player_action = self.keyboard.get_the_last_player_action()
-         if last_player_action.action_type is PlayerActionTypeEnum.NOTHING:
-             pass
-         elif last_player_action.action_type is PlayerActionTypeEnum.MOVE:
-             if last_player_action.direction is DirectionEnum.NO_DIRECTION:
+    def react_to_the_last_players_action(self, player_data: BodyData):
+        action = self.keyboard.get_the_last_player_action()
+        action_type = action.action_type
+        action_direction = action.direction
+        if action_type is PlayerActionTypeEnum.NOTHING:
+            pass
+        elif action_type is PlayerActionTypeEnum.MOVE:
+            if action_direction is DirectionEnum.NO_DIRECTION:
                 pass
-             elif last_player_action.direction is DirectionEnum.UP:
+            elif action_direction is DirectionEnum.UP:
                 player_data.coordinates.y += 1
-             elif last_player_action.direction is DirectionEnum.DOWN:
+            elif action_direction is DirectionEnum.DOWN:
                 player_data.coordinates.y += -1
-             elif last_player_action.direction is DirectionEnum.RIGHT:
+            elif action_direction is DirectionEnum.RIGHT:
                 player_data.coordinates.x += 1
-             elif last_player_action.direction is DirectionEnum.DOWN:
+            elif action_direction is DirectionEnum.DOWN:
                 player_data.coordinates.x += 1
-         elif last_player_action.action_type is PlayerActionTypeEnum.SHOOT:
-             if last_player_action.direction is DirectionEnum.NO_DIRECTION:
+        elif action_type is PlayerActionTypeEnum.SHOOT:
+            if action_direction is DirectionEnum.NO_DIRECTION:
                 pass
-             elif last_player_action.direction is DirectionEnum.UP:
+            elif action_direction is DirectionEnum.UP:
                 player_data.coordinates.y += 1
-             elif last_player_action.direction is DirectionEnum.DOWN:
+            elif action_direction is DirectionEnum.DOWN:
                 player_data.coordinates.y += -1
-             elif last_player_action.direction is DirectionEnum.RIGHT:
+            elif action_direction is DirectionEnum.RIGHT:
                 player_data.coordinates.x += 1
-             elif last_player_action.direction is DirectionEnum.DOWN:
+            elif action_direction is DirectionEnum.DOWN:
                 player_data.coordinates.x += 1
