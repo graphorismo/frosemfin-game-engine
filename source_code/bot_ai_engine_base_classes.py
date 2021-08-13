2ba229e0dd9b372ecbddde5e8e9f94c7d92b380b
graphorismo <graphorismo@gmail.com>
2021-08-14 02:09:41 +0300

    Add implementation of the bot ai engine base.
    1. Add IBotAiCore interface class to prevent creating
    of hard dependencies between bot ai engine and that ai realisation
    
    2. Add BotAction class to prevent using of tuple in IBotAiCore::get_actions_of_considered_bots(...)
    
    3. Add implementation of BotAiEngine. Its a basic class which will
    control and load data into and from IBotAiCore.

---
+++ source_code/bot_ai_engine_base_classes.py
@@ -0,0 +1,87 @@
+from source_code.common_classes import *
+from source_code.data_storage_base_classes import IDataStorage
+
+import copy
+
+
+class BotAction(BodyAction):
+    corresponded_bot: EntityData
+
+
+class IBotAiCore:
+
+    # Functions to override in inherited classes:
+
+    def consider_obstacles_data(self, obstacles_data: list[BodyData]):
+        raise RuntimeError("Can't find override for function"
+                           " consider_obstacle_data(...) from interface IBotAiCore")
+
+    def consider_bots_data(self, bots_data: list[BodyData]):
+        raise RuntimeError("Can't find override for function"
+                           " consider_bots_data(...) from interface IBotAiCore")
+
+    def consider_player_data(self, player_data: BodyData):
+        raise RuntimeError("Can't find override for function"
+                           " consider_player_data(...) from interface IBotAiCore")
+
+    def get_actions_of_considered_bots(self) -> list[BotAction]:
+        raise RuntimeError("Can't find override for function"
+                           " get_action_for_bots(...) from interface IBotAiCore")
+
+
+class BotAiEngine:
+    data_storage: IDataStorage
+    ai_core: IBotAiCore
+
+    projectiles_to_spawn: list[ProjectileData]
+
+    def __init__(self, data_storage: IDataStorage, ai_core: IBotAiCore):
+        self.data_storage = data_storage
+        self.ai_core = ai_core
+
+    def load_data_to_consider_by_ai(self):
+        bots_data_to_consider = self.data_storage.get_data_of_all_bodies_of_bots()
+        self.ai_core.consider_bots_data(bots_data_to_consider)
+        obstacles_data_to_consider = self.data_storage.get_data_of_all_bodies_of_obstacles()
+        self.ai_core.consider_obstacles_data(obstacles_data_to_consider)
+        player_data_to_consider = self.data_storage.get_data_of_the_body_of_the_player()
+        self.ai_core.consider_player_data(player_data_to_consider)
+
+    def calculate_and_perform_bots_actions(self):
+        actions = self.ai_core.get_actions_of_considered_bots()
+        for next_action in actions:
+            if next_action.action_type is BodyActionTypeEnum.NOTHING:
+                pass
+            elif next_action.action_type is BodyActionTypeEnum.MOVE:
+                self._move_the_body_correspond_to_the_move_action(next_action)
+            elif next_action.action_type is BodyActionTypeEnum.SHOOT:
+                self._spawn_a_projectile_correspond_to_the_shoot_action(next_action)
+
+    def _move_the_body_correspond_to_the_move_action(self, move_action: BotAction):
+        body_to_move = move_action.corresponded_bot
+        if move_action.direction is DirectionEnum.NO_DIRECTION:
+            pass
+        elif move_action.direction is DirectionEnum.UP:
+            body_to_move.coordinates.y += 1
+        elif move_action.direction is DirectionEnum.DOWN:
+            body_to_move.coordinates.y -= 1
+        elif move_action.direction is DirectionEnum.RIGHT:
+            body_to_move.coordinates.x += 1
+        elif move_action.direction is DirectionEnum.LEFT:
+            body_to_move.coordinates.y -= 1
+
+    def _spawn_a_projectile_correspond_to_the_shoot_action(self, shoot_action: BotAction):
+        new_projectile = ProjectileData()
+        new_projectile.coordinates = copy.deepcopy(shoot_action.corresponded_bot.coordinates)
+        new_projectile.direction = copy.deepcopy(shoot_action.direction)
+        if shoot_action.direction is DirectionEnum.NO_DIRECTION:
+            pass
+        elif shoot_action.direction is DirectionEnum.UP:
+            new_projectile.coordinates.y += 1
+        elif shoot_action.direction is DirectionEnum.DOWN:
+            new_projectile.coordinates.y -= 1
+        elif shoot_action.direction is DirectionEnum.RIGHT:
+            new_projectile.coordinates.x += 1
+        elif shoot_action.direction is DirectionEnum.LEFT:
+            new_projectile.coordinates.y -= 1
+
