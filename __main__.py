4ceaedd060b7a82a03358c0d90bd8a351e147d2d
graphorismo <graphorismo@gmail.com>
2021-08-16 00:00:05 +0300

    Add __main__.py containing main game script and empty __init__.py scripts in root and in source_code directories.
    To specify attachment cores of engines you must define them
    in make_and_configure_a_game_engine_builder(...) func instead of None

---
+++ __main__.py
@@ -0,0 +1,36 @@
+from source_code.game_engine_base_classes import *
+
+
+def make_and_configure_a_game_engine_builder() -> GameEngineBuilder:
+    new_game_engine_builder = GameEngineBuilder()
+
+    # Attach interfaces realisations here
+    new_game_engine_builder.data_storage = None
+    new_game_engine_builder.keyboard_reader = None
+    new_game_engine_builder.world_logic_core = None
+    new_game_engine_builder.bot_ai_core = None
+    new_game_engine_builder.gui_core = None
+
+    return new_game_engine_builder
+
+
+# Main script
+print("Attaching cores of engines")
+game_engine_builder = make_and_configure_a_game_engine_builder()
+print("Build the game engine")
+game_engine = game_engine_builder.build_a_game_engine()
+print("Initialise the game engine")
+game_engine.initialize()
+print("Running the game engine")
+while True:
+    game_engine.process_a_single_full_game_tick()
+print("Shutdown the game engine")
+game_engine.shutdown()
+print("Game engine stopped. You can exit safely.")
+
+
+
+
+
+
+
