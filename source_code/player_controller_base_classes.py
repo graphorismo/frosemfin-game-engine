b513ed9200b0805eb09ecda2caa2b10135221eab
graphorismo <graphorismo@gmail.com>
2021-08-15 17:21:06 +0300

    Readability and data flow changes. 1. Rename IKeyboard to IKeyboardReader for the better readability 2. Add IDataStorage field into PlayerController to get data of the player    from the inside of react_to_the_last_players_action(...)

--- source_code/player_controller_base_classes.py
+++ source_code/player_controller_base_classes.py
@@ -1,7 +1,9 @@
 from source_code.common_classes import *
 
+from source_code.data_storage_base_classes import IDataStorage
 
-class IKeyboard:
+
+class IKeyboardReader:
 
     def get_the_last_player_action(self) -> BodyAction:
         raise RuntimeError("Can't find override for function"
@@ -9,13 +11,16 @@ class IKeyboard:
 
 
 class PlayerController:
-    keyboard: IKeyboard
+    keyboard_reader: IKeyboardReader
+    data_storage: IDataStorage
 
-    def __init__(self, keyboard: IKeyboard):
-        self.keyboard = keyboard
+    def __init__(self, data_storage: IDataStorage, keyboard: IKeyboardReader):
+        self.keyboard_reader = keyboard
+        self.data_storage = data_storage
 
-    def react_to_the_last_players_action(self, player_data: BodyData):
-        action = self.keyboard.get_the_last_player_action()
+    def react_to_the_last_players_action(self):
+        player_data = self.data_storage.get_data_of_the_body_of_the_player()
+        action = self.keyboard_reader.get_the_last_player_action()
         action_type = action.action_type
         action_direction = action.direction
         if action_type is BodyActionTypeEnum.NOTHING:
