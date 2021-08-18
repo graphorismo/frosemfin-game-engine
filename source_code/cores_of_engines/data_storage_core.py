8c2b61985438317b95c2f7ada9c0ea828703e8c0
graphorismo <graphorismo@gmail.com>
2021-08-19 00:26:26 +0300

    Add a simple realisation of IDataStorage

--- source_code/cores_of_engines/data_storage_core.py
+++ source_code/cores_of_engines/data_storage_core.py
@@ -1,49 +1,52 @@
+from ..common_classes import *
+from ..data_storage_base_classes import IDataStorage
+from copy import copy
 
 
-class IDataStorage:
+class SimpleDataStorage(IDataStorage):
 
-    # Functions to override in inherited classes:
+    _projectiles: list[ProjectileData]
+    _bots: list[BodyData]
+    _obstacles: list[BodyData]
+    _player: BodyData
+    _collision_events: list[list[EntityData]]
 
     def get_data_of_all_entities(self) -> list[EntityData]:
-        raise RuntimeError("Can't find override for function"
-                           " get_data_of_all_entities(...) from interface IDataStorage")
+        all_entities_data = list[EntityData]
+        all_entities_data += self._projectiles
+        all_entities_data += self._bots
+        all_entities_data += self._obstacles
+        all_entities_data += self._player
+        return copy(all_entities_data)
 
     def get_data_of_all_projectiles(self) -> list[ProjectileData]:
-        raise RuntimeError("Can't find override for function"
-                           " get_data_of_projectiles(...) from interface IDataStorage")
+        return copy(self._projectiles)
 
     def add_data_of_projectiles(self, projectiles: list[ProjectileData]):
-        raise RuntimeError("Can't find override for function"
-                           " edit_or_create_data_of_projectiles(...) from interface IDataStorage")
+        self._projectiles += projectiles
 
     def get_data_of_all_bodies_of_bots(self) -> list[BodyData]:
-        raise RuntimeError("Can't find override for function"
-                           " get_data_of_bodies_of_bots(...) from interface IDataStorage")
+        return copy(self._bots)
 
     def add_data_of_bodies_of_bots(self, bodies_of_bots: list[BodyData]):
-        raise RuntimeError("Can't find override for function"
-                           " edit_or_create_data_of_bodies_of_bots(...) from interface IDataStorage")
+        self._bots += bodies_of_bots
 
     def get_data_of_all_bodies_of_obstacles(self) -> list[BodyData]:
-        raise RuntimeError("Can't find override for function"
-                           " get_data_of_all_bodies_of_obstacles(...) from interface IDataStorage")
+        return copy(self._obstacles)
 
-    def add_data_of_bodies_of_obstacles(self, bodies_of_bots: list[BodyData]):
-        raise RuntimeError("Can't find override for function"
-                           " edit_or_create_data_of_bodies_of_obstacles(...) from interface IDataStorage")
+    def add_data_of_bodies_of_obstacles(self, bodies_of_obstacles: list[BodyData]):
+        self._obstacles += bodies_of_obstacles
 
     def get_data_of_the_body_of_the_player(self) -> BodyData:
-        raise RuntimeError("Can't find override for function"
-                           " get_data_of_the_body_of_the_player(...) from interface IDataStorage")
+        return copy(self._player)
 
     def add_the_player_body_data(self, player_body_data: BodyData):
-        raise RuntimeError("Can't find override for function"
-                           " edit_the_player_body_data(...) from interface IDataStorage")
+        self._player = player_body_data
 
     def push_collisions_data(self, collisions_data: list[list[EntityData]]):
-        raise RuntimeError("Can't find override for function"
-                           " push_collisions_data(...) from interface IDataStorage")
+        self._collision_events += collisions_data
 
     def pop_collisions_data(self, collisions_data: list[list[EntityData]]):
-        raise RuntimeError("Can't find override for function"
-                           " pop_collisions_data(...) from interface IDataStorage")
+        poped_events = self._collision_events
+        self._collision_events = []
+        return poped_events
