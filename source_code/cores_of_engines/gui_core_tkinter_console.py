2048f33eea8a0c2d445404fb6546728e524fce50
graphorismo <graphorismo@gmail.com>
2021-08-20 01:22:31 +0300

    Fix fill area func. Optimize the case of resolution = 1 by adding check for that case in draw funcs and calling a new draw func which is simplified in compare to fill area function

--- source_code/cores_of_engines/gui_core_tkinter_console.py
+++ source_code/cores_of_engines/gui_core_tkinter_console.py
@@ -5,8 +5,8 @@ from ..common_classes import *
 
 class DrawBuffer:
     lines: list[str] = [str() for _ in range(10)]
-    _high: int = 10
-    _width: int = 10
+    _high: int
+    _width: int
 
     basic_filler_symbol = '.'
 
@@ -23,12 +23,20 @@ class DrawBuffer:
         for h in range(self._high):
             self.lines[h] = self.basic_filler_symbol[0]*self._width
 
+    def draw_a_symbol_on_coordinates(self, symbol: str, coordinates: Vector2i):
+        line_old_data = self.lines[coordinates.y]
+        self.lines[coordinates.y] = line_old_data[0 : coordinates.x - 1] + \
+                                 symbol[0] +\
+                                 line_old_data[coordinates.x+1 : ]
+
+
     def fill_the_area_with_symbol(self, area_defining_vector_1: Vector2i, area_defining_vector_2: Vector2i,
                                   area_filler_symbol: str) -> str:
         top_line_index: int
         bottom_line_index: int
         left_edge_column_index: int
         right_edge_column_index: int
+
         # Define purposes of coordinates of vectors
         if area_defining_vector_1.y >= area_defining_vector_2.y:
             top_line_index = area_defining_vector_1.y
@@ -42,18 +50,20 @@ class DrawBuffer:
         else:
             right_edge_column_index = area_defining_vector_2.x
             left_edge_column_index = area_defining_vector_1.x
+
         # Snapping of coordinates of vectors
         top_line_index = self._clip_number_to_fit_the_high_and_return_copy(top_line_index)
         bottom_line_index = self._clip_number_to_fit_the_high_and_return_copy(bottom_line_index)
         left_edge_column_index = self._clip_number_to_fit_the_width_and_return_copy(left_edge_column_index)
         right_edge_column_index = self._clip_number_to_fit_the_width_and_return_copy(right_edge_column_index)
+
         # Filling the area
-        area_width = right_edge_column_index - left_edge_column_index
-        for line_index in range(bottom_line_index, top_line_index):
+        area_width = right_edge_column_index - left_edge_column_index + 1
+        for line_index in range(bottom_line_index, top_line_index+1):
             line_old_data = self.lines[line_index]
-            self.lines[line_index] = line_old_data[0:left_edge_column_index] + \
-                                     area_filler_symbol * area_width + \
-                                     line_old_data[right_edge_column_index:]
+            self.lines[line_index] = line_old_data[0:left_edge_column_index-1] + \
+                                     area_filler_symbol[0] * area_width + \
+                                     line_old_data[right_edge_column_index+1:]
 
     def _clip_number_to_fit_the_width_and_return_copy(self, number_to_clip: int) -> int:
         clipped_number: int
@@ -89,7 +99,8 @@ class SimpleWindow(tk.Frame):
         self.text_graphics_field.grid()
 
     def show_draw_buffer(self, draw_buffer: DrawBuffer):
-        self.text_graphics_field.insert(str(draw_buffer))
+        self.text_graphics_field.delete('0.0')
+        self.text_graphics_field.insert('0.0', str(draw_buffer))
 
 
 class TextModeGraphicTkinterGUIDrawer(IGUIDrawer):
@@ -108,28 +119,39 @@ class TextModeGraphicTkinterGUIDrawer(IGUIDrawer):
 
     def add_an_obstacle_to_the_draw_buffer(self, obstacle_data: BodyData):
         self._raise_an_exception_if_an_entity_have_negative_coordinates(obstacle_data)
-        vector1, vector2 = \
-            self._get_two_vectors_defining_draw_area_with_center_on_coordinates(obstacle_data.coordinates)
-        self._draw_buffer.fill_the_area_with_symbol(vector1, vector2, '\u25a6')
-
+        if self._resolution_of_draw > 1:
+            vector1, vector2 = \
+                self._get_two_vectors_defining_draw_area_with_center_on_coordinates(obstacle_data.coordinates)
+            self._draw_buffer.fill_the_area_with_symbol(vector1, vector2, '\u25a6')
+        else:
+            self._draw_buffer.draw_a_symbol_on_coordinates('\u25a6', obstacle_data.coordinates)
 
     def add_a_projectile_to_the_draw_buffer(self, projectile_data: ProjectileData):
         self._raise_an_exception_if_an_entity_have_negative_coordinates(projectile_data)
-        vector1, vector2 = \
-            self._get_two_vectors_defining_draw_area_with_center_on_coordinates(projectile_data.coordinates)
-        self._draw_buffer.fill_the_area_with_symbol(vector1, vector2, '\u2600')
+        if self._resolution_of_draw > 1:
+            vector1, vector2 = \
+                self._get_two_vectors_defining_draw_area_with_center_on_coordinates(projectile_data.coordinates)
+            self._draw_buffer.fill_the_area_with_symbol(vector1, vector2, '\u2600')
+        else:
+            self._draw_buffer.draw_a_symbol_on_coordinates('\u2600', projectile_data.coordinates)
 
     def add_a_bot_to_the_draw_buffer(self, bot_data: BodyData):
         self._raise_an_exception_if_an_entity_have_negative_coordinates(bot_data)
-        vector1, vector2 = \
-            self._get_two_vectors_defining_draw_area_with_center_on_coordinates(bot_data.coordinates)
-        self._draw_buffer.fill_the_area_with_symbol(vector1, vector2, '\u25cf')
+        if self._resolution_of_draw > 1:
+            vector1, vector2 = \
+                self._get_two_vectors_defining_draw_area_with_center_on_coordinates(bot_data.coordinates)
+            self._draw_buffer.fill_the_area_with_symbol(vector1, vector2, '\u25cf')
+        else:
+            self._draw_buffer.draw_a_symbol_on_coordinates('\u25cf', bot_data.coordinates)
 
     def add_a_player_to_the_draw_buffer(self, player_data: BodyData):
         self._raise_an_exception_if_an_entity_have_negative_coordinates(player_data)
-        vector1, vector2 = \
-            self._get_two_vectors_defining_draw_area_with_center_on_coordinates(player_data.coordinates)
-        self._draw_buffer.fill_the_area_with_symbol(vector1, vector2, '\u25c9')
+        if self._resolution_of_draw > 1:
+            vector1, vector2 = \
+                self._get_two_vectors_defining_draw_area_with_center_on_coordinates(player_data.coordinates)
+            self._draw_buffer.fill_the_area_with_symbol(vector1, vector2, '\u25c9')
+        else:
+            self._draw_buffer.draw_a_symbol_on_coordinates('\u25c9', player_data.coordinates)
 
     def set_the_draw_resolution(self, units_of_picture_per_one_unit_of_coordinates: int):
         if units_of_picture_per_one_unit_of_coordinates <= 0:
