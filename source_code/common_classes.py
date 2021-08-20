346761c532436f62160e74fd39fd369454f8e991
graphorismo <graphorismo@gmail.com>
2021-08-20 16:21:30 +0300

    Add constructors, unique ids and pool for them. 1. Add a mandatory field unique_id into the base UniqueEntity class 2. Add constructors with keyword arguments

--- source_code/common_classes.py
+++ source_code/common_classes.py
@@ -14,25 +14,35 @@ class Vector2i:
     x: int
     y: int
 
-    def __init__(self):
-        self.x = 0
-        self.y = 0
+    def __init__(self, *, x: int = 0, y: int = 0):
+        self.x = x
+        self.y = y
+
 
 class UniqueIDPool:
-    pool_of_free_ids: set[int]
-    pool_of_locked_ids: set[int]
-    size: int
+    _pool_of_free_ids: set[int]
+    _pool_of_locked_ids: set[int]
+    _size: int
 
-    def __init__(self, size: int):
-        self.pool_of_free_ids = set()
-        self.pool_of_locked_ids = set()
+    def __init__(self, *, size: int = 10):
+        self._pool_of_free_ids = set()
+        self._pool_of_locked_ids = set()
         for i in range(1, size+1):
-            self.pool_of_free_ids.add(i)
+            self._pool_of_free_ids.add(i)
+        self._size = size
 
     def pop_unique_id(self) -> int:
-        poped_id = self.pool_of_free_ids.pop()
-        self.pool_of_locked_ids.add(poped_id)
+        poped_id = self._pool_of_free_ids.pop()
+        self._pool_of_locked_ids.add(poped_id)
         return poped_id
+
+    def push_an_unique_id_back(self, pushed_id: int):
+        if pushed_id in self._pool_of_locked_ids:
+            self._pool_of_locked_ids.remove(pushed_id)
+            self._pool_of_free_ids.add(pushed_id)
+        else:
+            raise RuntimeError("Tried to push back an id were not in the id-pool before"
+                               "or is already freed")
 
 
 class UniqueEntity:
@@ -48,9 +58,9 @@ class UniqueEntity:
 class EntityData(UniqueEntity):
     coordinates: Vector2i
 
-    def __init__(self):
-        super().__init__()
-        self.coordinates = Vector2i()
+    def __init__(self, unique_id: int, *, x: int = 0, y: int = 0):
+        super().__init__(unique_id)
+        self.coordinates = Vector2i(x=x, y=y)
 
 
 class DirectionEnum(Enum):
@@ -79,14 +89,16 @@ class BodyAction:
 class ProjectileData(EntityData):
     direction: DirectionEnum
 
-    def __init__(self):
-        super().__init__()
-        self.direction = DirectionEnum.NO_DIRECTION
+    def __init__(self, unique_id: int, *, x: int = 0, y: int = 0,
+                 direction: DirectionEnum = DirectionEnum.NO_DIRECTION):
+        super().__init__(unique_id, x=x, y=y)
+        self.direction = direction
 
 
 class BodyData(EntityData):
     hit_points: int
 
-    def __init__(self):
-        super().__init__()
-        self.hit_points = 0
+    def __init__(self, unique_id: int, *, x: int = 0, y: int = 0,
+                 hp: int = 1):
+        super().__init__(unique_id, x=x, y=y)
+        self.hit_points = hp
